986cb6d42fd273ee5fae5a77ae5fbee8f8d4b7b2
Sledz-s <86799405+Sledz-s@users.noreply.github.com>
2026-03-29 12:40:27 +0200

    Update track.h

--- inc/track.h
+++ inc/track.h
@@ -7,6 +7,11 @@ extern const uint8_t CV3_prelude_ch1[];
 extern const uint8_t CV3_prelude_ch2[];
 extern const uint8_t CV3_prelude_ch3[];
 
+extern const uint8_t octave_change[];
+extern const uint8_t triad_up[];
+extern const uint8_t test_bass[];
+extern const uint8_t empty_track[1];
+
 // ASM
 extern const uint8_t loop_test[];
 extern const uint8_t drum_loop[];
